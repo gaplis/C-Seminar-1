75ecf231822b1cff3dc058519510f3408bed92ff
IlyaChistov <gaplis322@gmail.com>
2022-09-01 15:07:38 +0300

    Seminar 3

--- Seminar_3/001/Program.cs
+++ Seminar_3/001/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X != 0 и Y != 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+else
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+else
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
---
+++ Seminar_3/004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int one = 1;
+
+while (one <= n)
+{
+    int result = one * one;
+    Console.Write($"{result} ");
+    one++;
+}
+
+// for (int one = 1; one <= n; one++)
+// {
+//     Console.Write($"{result} ");
+// }
